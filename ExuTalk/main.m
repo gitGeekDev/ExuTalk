7889d47efe0861d7b301a0a4042de211dacb024d
gitGeekDev <manish.mcaipu@gmail.com>
2014-07-13 22:36:29 +0530

    Initial Commit

---
+++ ExuTalk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExuTalk
+//
+//  Created by Manish Kumar on 13/07/14.
+//  Copyright (c) 2014 Manish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
